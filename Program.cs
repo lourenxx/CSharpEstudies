c722c134beb54f7992d535b1ad57b74b191a3d68
lourenxx <guilhermepxl7@gmail.com>
2023-09-17 12:08:39 -0300

    adding the product 6 and 2 payment method session

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ class Program
         int productQuant;
         string payment; 
 
-        Console.WriteLine("enter the product code: ");
+        Console.WriteLine("enter the product code [2, 5 or 6]: ");
         productCode = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("enter the the product quantity: ");
         productQuant = Convert.ToInt32(Console.ReadLine());
@@ -18,6 +18,60 @@ class Program
         if (productCode == 5)
         {
             double finalValue = 32 * productQuant;
+            if (payment == "card")
+            {
+                if (productQuant >= 10)
+                {
+                    double discountValue = finalValue * 0.08;
+                    Console.WriteLine($"the final valor with the discount now is: {discountValue}");
+                }
+
+                else 
+                {
+                    double discountValue = finalValue * 0.02;
+                    Console.WriteLine($"the final valor with the discount now is: {discountValue}");
+                }
+            }
+            if (payment == "money")
+            {
+                if (finalValue >= 500)
+                {
+                    double discountValue = finalValue * 0.15;
+                    Console.WriteLine($"the final valor with the discount now is: {discountValue}");
+                }
+            }
+        }
+
+         if (productCode == 6)
+        {
+            double finalValue = 45 * productQuant;
+            if (payment == "card")
+            {
+                if (productQuant >= 10)
+                {
+                    double discountValue = finalValue * 0.08;
+                    Console.WriteLine($"the final valor with the discount now is: {discountValue}");
+                }
+
+                else 
+                {
+                    double discountValue = finalValue * 0.02;
+                    Console.WriteLine($"the final valor with the discount now is: {discountValue}");
+                }
+            }
+            if (payment == "money")
+            {
+                if (finalValue >= 500)
+                {
+                    double discountValue = finalValue * 0.15;
+                    Console.WriteLine($"the final valor with the discount now is: {discountValue}");
+                }
+            }
+        }
+
+         if (productCode == 2)
+        {
+            double finalValue = 27 * productQuant;
             if (payment == "card")
             {
                 if (productQuant >= 10)
